921b9aad4afd7d33cbfc3b395820403e862dfe29
kunalpachori <54906343+kunalpachori@users.noreply.github.com>
2019-12-01 00:50:03 -0600

    Changes in py file for travis ci

--- HR_Analysis.py
+++ HR_Analysis.py
@@ -2,12 +2,13 @@
 # HR data analysis using dataset from IBM, UCI, U.S. Census Reports and Bureau of Labor Statistics
 
 
-def print_value(value):
+def print_value(a, b):
     """
-    >>> print_value('3')
-    >>> value_1 = 1
-    >>> print_value(value1)
+    >>> print_value(4, 8)
+    >>> value_1 = 9
+    >>> print_value(value_1, 0)
     """
-    print(value)
+    return a+b
 
-print(print_value('2'))
+
+print(print_value(2, 5))
