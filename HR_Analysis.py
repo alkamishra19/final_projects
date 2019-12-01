89128b093cefe5f0be0ec1cd9de7984b1c82dac9
kunalpachori <54906343+kunalpachori@users.noreply.github.com>
2019-12-01 00:55:42 -0600

    Changes in py file for travis ci

--- HR_Analysis.py
+++ HR_Analysis.py
@@ -5,10 +5,12 @@
 def print_value(a, b):
     """
     >>> print_value(4, 8)
+    12
     >>> value_1 = 9
     >>> print_value(value_1, 0)
+    9
     """
     return a+b
 
 
-print(print_value(2, 5))
+print_value(2, 5)
